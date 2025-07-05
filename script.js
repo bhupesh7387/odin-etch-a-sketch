7e91729b5c0b4ea394adcbed4e8cfd1f41c60980
Bhupesh Rawat <75159891+bhupesh7387@users.noreply.github.com>
2025-07-05 18:15:25 +0530

    Improve style and grid size button

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const grid = document.querySelector("#container");
 
-for (let i = 0; i < 16 * 16; i++) {
-    const gridDiv = document.createElement("div");
-    gridDiv.className = "grid-div";
-    grid.appendChild(gridDiv);
+function generateGrid(gridSize) {
+    if (gridSize > 100 || gridSize === null || gridSize === "") {
+        return;
+    }
+    grid.innerHTML = "";
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const gridDiv = document.createElement("div");
+        gridDiv.className = "grid-div";
+        gridDiv.style.width = `${800 / gridSize}px`;
+        gridDiv.style.height = `${800 / gridSize}px`;
+        grid.appendChild(gridDiv);
+    }
 }
+
+generateGrid(16);
 
 grid.addEventListener("mouseover", (e) => {
     e.target.style.backgroundColor = "black";
+});
+
+const girdSizeButton = document.querySelector("#grid-size-btn");
+girdSizeButton.addEventListener("click", () => {
+    const gridSize = prompt("Enter grid size(Max. 100)");
+    generateGrid(gridSize);
 });
